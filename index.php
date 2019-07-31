d931924033f92eb860912ab109b37d8380a690b3
adsliymis <info@barisyeman.com>
2019-07-31 10:18:46 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+﻿<?php 
+   $api="GET_YOUR_GOOLE_API";
+   if(@$_GET["lang"]){
+      $lang=$_GET["lang"];
+   }
+   else{
+      $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2);
+   }
+   $link=file_get_contents("https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=15&regionCode=".$lang."&key=".$api."");
+   $json=json_decode($link,true);
+   for ($i=0; $i < 5; $i++) {     
+      $json["items"][$i]["snippet"]["thumbnails"]["maxres"]["url"]; //Video Thumb URL
+      $json["items"][$i]["snippet"]["title"] //Video Tittle
+      echo "https://youtu.be/".$json["items"][$i]["id"]; //Video URL
+      $json["items"][$i]["snippet"]["description"] //Get Video Description
+}
